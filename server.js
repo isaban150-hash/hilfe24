5c94bd5686c534cc8852fe4c893bbaea3142be90
isaban150-hash <isaban150@gmail.com>
2026-04-19 22:48:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+ res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/test", (req, res) => {
