674e9db9c16a6175c8bbe7f1df10a085875b0b39
isaban150-hash <isaban150@gmail.com>
2026-04-19 19:39:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
-app.use(express.static(__dirname));
+app.use(express.static(process.cwd()));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(process.cwd(), "index.html"));
 });
 
 function getLanguageName(lang) {
