79042bbedefded41060e2161c2c40aff7e89d556
isaban150-hash <isaban150@gmail.com>
2026-04-19 20:28:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("hilfe24 läuft ✅");
+  res.send("OK");
 });
 
 app.listen(PORT, () => {
-  console.log("Server läuft auf Port " + PORT);
+  console.log("läuft");
 });
