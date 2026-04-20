33567c21a6121baa89860950d9cff1e8ab4debdd
isaban150-hash <isaban150@gmail.com>
2026-04-20 12:49:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Server läuft sauber");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
 app.get("/test", (req, res) => {
-  res.json({ ok: true });
+  res.json({ ok: true, message: "Server läuft sauber" });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
