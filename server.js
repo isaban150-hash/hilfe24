758e12c3e639448b7a150b2639f217bd009c1379
isaban150-hash <isaban150@gmail.com>
2026-04-20 12:02:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,15 +18,6 @@ app.get("/test", (req, res) => {
 app.listen(PORT, () => {
   console.log("Server läuft auf Port " + PORT);
 });
-    const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + GEMINI_API_KEY,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          contents: [
             {
               parts: [{ text: "Sag mir Hallo auf Deutsch" }],
             },
