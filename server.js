ee4ca90b2b7ccbd30088508259cf974a7e3db614
isaban150-hash <isaban150@gmail.com>
2026-04-20 11:03:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server läuft auf Port " + PORT);
+});
 app.use(cors());
 app.use(express.json());
 
